738a275649a18a05cad66d0adafd72a4dea31d51
ayax5562001@gmail.com <ayax5562001@gmail.com>
2024-02-09 11:08:03 -0600

    useCrud configurandolo

---
+++ src/hooks/useCrud.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { useState } from "react";
+
+
+const useCrud = (urlBase) => {
+  const [apiData, setapiData] = useState();
+
+  const getApi = () =>{
+    axios.get();
+  }
+}
+
+export default useCrud;
