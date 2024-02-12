e40255a0a41462c1accdfb3e7023b8308c885a57
ayax5562001@gmail.com <ayax5562001@gmail.com>
2024-02-12 17:08:06 -0600

    formUser

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import useCrud from './hooks/useCrud';
 
 function App() {
-  const url = "https://users-crud.academlo.tech/swagger/"
+  const url = "https://users-crud.academlo.tech/";
+  const [users, getUsers, createUser, deleteUser, updateUser] = useCrud(url);
 
+  useEffect(() => {
+   getUsers('/users/');
+  }, []);
+  console.log(users);
+  
   return (
    <div>
     <h1>Crud de usuarios</h1>
--- src/hooks/useCrud.js
+++ src/hooks/useCrud.js
@@ -2,45 +2,45 @@ import axios from "axios";
 import { useState } from "react";
 
 
+
 const useCrud = (urlBase) => {
+  const [apiData, setapiData] = useState([]);
 
-  const [apiData, setapiData] = useState();
-
-  const getApi = (path) =>{
+  const getApi = (path) => {
     axios.get(`${urlBase}${path}/`)
-        .then(res => apiData(res.data))
-        .catch(err => console.log(err));
-  }
+    .then(res => setapiData(res.data)) // Aquí también había un error, debe ser setapiData, no apiData
+    .catch(err => console.log(err));
+}
 
-  const postApi = (path, data) => {
-    axios.post(`${urlBase}${path}/`, data)
+const postApi = (path, data) => {
+  axios.post(`${urlBase}${path}/`, data)
     .then(res => {
       setapiData([...apiData, res.data]);
       console.log(res.data);
     })
     .catch(err => console.log(err));
-  }
+}
 
-  const deleteApi = (path, id) => {
-    axios.delete(`${urlBase}${path}/${id}/ `)
-      .then(() => {
-        setapiData(apiData.filter(element => element.id!==id));
-        console.log('borrado con exito');
-      })
-      .catch(err => console.log(err));
+const deleteApi = (path, id) => {
+  axios.delete(`${urlBase}${path}/${id}/`)
+    .then(() => {
+      setapiData(apiData.filter(element => element.id !== id));
+      console.log('borrado con éxito');
+    })
+    .catch(err => console.log(err));
+}
 
-      //update
-      const updateApi = (path, id, data) => {
-        axios.patch(`${urlBase}${path}/${id}/`, data)
-        .then(res => {
-          setapiData(apiData.map(element => element.id===id? res.data : element));
-          console.log(res.data)
-        })
-        .catch(err => console.log(err));
+// Función updateApi movida fuera de deleteApi
+const updateApi = (path, id, data) => {
+  axios.patch(`${urlBase}${path}/${id}/`, data)
+    .then(res => {
+      setapiData(apiData.map(element => element.id === id ? res.data : element));
+      console.log(res.data);
+    })
+    .catch(err => console.log(err));
+}
 
-      }
-  }
-  return [apiData, getApi, postApi, deleteApi, updateApi];
+return [apiData, getApi, postApi, deleteApi, updateApi];
 }
 
 export default useCrud;
