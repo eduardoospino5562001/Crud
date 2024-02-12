602b8f8b13c8a639beb2b2e96cff2cb921aeb5b7
ayax5562001@gmail.com <ayax5562001@gmail.com>
2024-02-12 16:18:59 -0600

    configuracion update_ useCrud

--- src/hooks/useCrud.js
+++ src/hooks/useCrud.js
@@ -3,13 +3,44 @@ import { useState } from "react";
 
 
 const useCrud = (urlBase) => {
+
   const [apiData, setapiData] = useState();
 
   const getApi = (path) =>{
-    axios.get(`${urlBase}${path}`)
+    axios.get(`${urlBase}${path}/`)
         .then(res => apiData(res.data))
         .catch(err => console.log(err));
   }
+
+  const postApi = (path, data) => {
+    axios.post(`${urlBase}${path}/`, data)
+    .then(res => {
+      setapiData([...apiData, res.data]);
+      console.log(res.data);
+    })
+    .catch(err => console.log(err));
+  }
+
+  const deleteApi = (path, id) => {
+    axios.delete(`${urlBase}${path}/${id}/ `)
+      .then(() => {
+        setapiData(apiData.filter(element => element.id!==id));
+        console.log('borrado con exito');
+      })
+      .catch(err => console.log(err));
+
+      //update
+      const updateApi = (path, id, data) => {
+        axios.patch(`${urlBase}${path}/${id}/`, data)
+        .then(res => {
+          setapiData(apiData.map(element => element.id===id? res.data : element));
+          console.log(res.data)
+        })
+        .catch(err => console.log(err));
+
+      }
+  }
+  return [apiData, getApi, postApi, deleteApi, updateApi];
 }
 
 export default useCrud;
